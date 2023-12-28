68da0fbe16293a697616b4d30d3a48df6fd934c4
unknown <amandhanwaniofficial@gmail.com>
2023-12-28 12:18:10 +0530

    second commit

--- src/nav.js
+++ src/nav.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/",(req,res)=>{
-    res.send("hello express form the home page");
+    res.send("hello express form the home page change");
 })
 app.get("/about",(req,res)=>{
     res.send("hello express form the about page");
